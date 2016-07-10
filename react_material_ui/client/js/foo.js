bd484f030a33ef06ef82e618a376d40a9fbec789
Alessio Rapini <alessio.rapini@yahoo.it>
2016-07-10 20:17:18 +0200

    override default inline styles of a mui compononent

--- react_material_ui/client/js/foo.js
+++ react_material_ui/client/js/foo.js
@@ -1,16 +1,16 @@
 import React from 'react';
 // Import a palette color that you can change the color of components
-import {cyan500} from 'material-ui/styles/colors';
+import {gray500} from 'material-ui/styles/colors';
 // Import the Material UI default theme
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // Get the overridable properties of the Material UI theme
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 // Import the AppBar component
-import AppBar from 'material-ui/AppBar';
+import Checkbox from 'material-ui/Checkbox';
 
 const muiTheme = getMuiTheme({
     palette: {
-        textColor: cyan500
+        textColor: gray500
     },
     appBar: {
         height: 50
@@ -22,7 +22,18 @@ class Foo extends React.Component {
         return (
             // Take the customized properties of the theme
             <MuiThemeProvider muiTheme={muiTheme}>
-                <AppBar title="My AppBar" />
+                <Checkbox
+                    id="checkboxId1"
+                    name="checkboxName1"
+                    value="checkboxValue1"
+                    label="went for a run today"
+                    style={{
+                        width: '50%',
+                        margin: '0 auto'
+                    }}
+                    iconStyle={{
+                        fill: '#FF4081'
+                    }} />
             </MuiThemeProvider>
         );
     }
